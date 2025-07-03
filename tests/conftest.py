48d93db1663f6584ae95fd2ab0262429699e53a5
DaYascaribay <david-ldu03@hotmail.com>
2025-07-03 12:05:33 -0500

    feat:Add env variables for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,13 @@
+import os
 import pytest
-from run import app as flask_app 
+from run import app as flask_app
 
 @pytest.fixture
 def client():
+    # ✅ Set environment variables for testing
+    os.environ["ORDER_CREATION_URL"] = "http://localhost:5000/order"
+    os.environ["ORDER_STATUS_URL"] = "http://localhost:5000/status"
+
     flask_app.config["TESTING"] = True
     with flask_app.test_client() as client:
         yield client
-
